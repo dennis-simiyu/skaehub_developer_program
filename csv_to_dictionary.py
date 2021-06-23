b0fe88bf5c044e566d76084a91b1f9271123bbde
dennis-simiyu <simiyudennis142@gmail.com>
2021-06-23 09:08:20 -0500

    boot camp assignment

--- csv_to_dictionary.py
+++ csv_to_dictionary.py
@@ -1,5 +1,8 @@
 import csv
-with open("team.csv", 'r') as file:
-    csv_file = csv.DictReader(file)
-    for row in csv_file:
-        print(dict(row))
+def cvReader():
+    with open("team.csv", 'r') as file:
+        csv_file = csv.DictReader(file)
+        for row in csv_file:
+            print(dict(row))
+
+cvReader()
--- timeout_request.py
+++ timeout_request.py
@@ -1,14 +1,16 @@
 import requests
-print("timeout = 0.001")
-try:
-    r = requests.get('https://api.twitter.com/2/tweets/', timeout = 0.001)
-    print(r.text)
-except requests.exceptions.RequestException as e:
-    print(e)    
+def requestTimeout():
+        print("timeout = 0.001")
+        try:
+            r = requests.get('https://api.twitter.com/2/tweets/', timeout = 0.001)
+            print(r.text)
+        except requests.exceptions.RequestException as e:
+            print(e)    
 
-print("\ntimeout = 1.0")    
-try:
-    r = requests.get('https://api.twitter.com/2/tweets/', timeout = 1.0)
-    print("Connected....!")
-except requests.exceptions.RequestException as e:
-    print(e)
+        print("\ntimeout = 1.0")    
+        try:
+            r = requests.get('https://api.twitter.com/2/tweets/', timeout = 1.0)
+            print("Connected....!")
+        except requests.exceptions.RequestException as e:
+            print(e)
+requestTimeout()
